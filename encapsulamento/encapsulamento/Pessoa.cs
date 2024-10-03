12278d89613a1a6549dc2301f903cdfc9fa53d48
FernandaNascimentoDaSilva <fn94695@gmail.com>
2024-10-03 16:51:07 -0300

    Add files via upload

---
+++ encapsulamento/encapsulamento/Pessoa.cs
@@ -0,0 +1,35 @@
+﻿namespace encapsulamento
+{
+    public class Pessoa
+    {
+        private string nome;
+        private int idade;
+        private string telefone;
+        private string email;
+        private string endereco;
+
+
+ 
+        public string Nome {
+            get { return (string) nome; }
+            set { if (value != "") nome = value; }
+        }
+
+        public int Idade {
+            get { return (int) idade; }
+            set { if(value > 0) idade = value; }
+        }
+        public string Telefone {
+            get { return (string) telefone; }
+            set { if(value != "") telefone = value; }
+        }
+        public string Email {
+            get { return (string) email; }
+            set { if (value != "") email = value; }
+        }
+        public string Endereco {
+            get { return endereco; }
+            set { if (value != "") endereco = value; }
+        }
+    }
+}
---
+++ encapsulamento/encapsulamento/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using encapsulamento;
+
+Pessoa p = new Pessoa();
+
+
+p.Nome = "Fernanda";
+p.Idade = 27;
+p.Idade = -0;
+p.Telefone = "81 986741517";
+p.Email = "aluno.senai@gmail.com";
+p.Endereco = "Antiga PE 28, s/n - Jardim Santo Inacio, PE, 54500-000";
+
+
+Console.WriteLine($"O nome dessa pessoa é: {p.Nome}");
+Console.WriteLine($"Essa pessoa tem {p.Idade} anos");
+Console.WriteLine($"O Telefone dessa pessoa é {p.Telefone}");
+Console.WriteLine($"O email dessa pessoa é {p.Email}");
+Console.WriteLine($"O endereço dessa pessoa é {p.Endereco}");
+
+Console.ReadKey();
